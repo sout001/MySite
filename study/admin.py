6888f618dd15bcea00829c9585263b554c13aa0d
sout001 <642829931@qq.com>
2020-12-07 05:37:04 +0800

    1、新增用户板块

--- study/admin.py
+++ study/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
+from study.models import BaseModel, User
+
+
+class BaseAdmin(admin.ModelAdmin):
+    exclude = ['create_time', 'update_time', 'delete_time']
+    list_per_page = 10
+
+
+@admin.register(User)
+class UserAdmin(BaseAdmin):
+    verbose_name = "用户"
+    list_display = ['id', 'user_id', 'user_name', 'user_pwd', 'user_phone', 'user_email', 'real_name', 'user_sex']
+
 
 admin.site.site_header = "案例管理后台"
 admin.site.site_title = '案例管理后台'
--- study/models.py
+++ study/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    create_time = models.DateTimeField(verbose_name="创建时间", default=timezone.now)
+    update_time = models.DateTimeField(verbose_name="修改时间", default=timezone.now)
+    delete_time = models.DateTimeField(verbose_name="删除时间", default=timezone.now)
+    enable = models.BooleanField(verbose_name="有效", default=1)
+
+    class Meta:
+        abstract = True
+
+
+class User(BaseModel):
+    user_id = models.CharField(verbose_name="用户uid", max_length=50)
+    user_name = models.CharField(verbose_name="用户名称", max_length=50)
+    user_pwd = models.CharField(verbose_name="用户密码", max_length=18)
+    user_phone = models.CharField(verbose_name="用户手机", max_length=11)
+    user_email = models.CharField(verbose_name="用户邮箱", max_length=20)
+    real_name = models.CharField(verbose_name="真实姓名", max_length=20)
+    user_sex = models.IntegerField(verbose_name="性别", choices=((0, "男"), (1, "女")), default=1)
+
+    class Meta:
+        verbose_name = "用户"
+        verbose_name_plural = "用户"
+        db_table = "user"
+
